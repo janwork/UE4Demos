ae722b63a2e66217a89e34f2c0f2b0a1f93462fd
janwork@163.com <janwork@163.com>
2021-12-22 14:57:47 +0800

    动画里添加技能释放通知

---
+++ Source/ActionRPGTest/Public/Abilities/RPGAbilityTaskPlayMontageAndWait.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/Tasks/AbilityTask_PlayMontageAndWait.h"
+#include "RPGAbilityTaskPlayMontageAndWait.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONRPGTEST_API URPGAbilityTaskPlayMontageAndWait : public UAbilityTask_PlayMontageAndWait
+{
+	GENERATED_BODY()
+	
+};
